27b1554e6bd2914155c5403ea72de2412d04de58
Ricky-yu <ts00484515@gmail.com>
2020-08-17 23:20:41 +0900

    add AuthViewController and fix BaseController

---
+++ Todo/Todo/Controller/Auth/AuthViewController.swift
@@ -0,0 +1,49 @@
+//
+//  AuthViewController.swift
+//  Todo
+//
+//  Created by CHEN SINYU on 2020/08/17.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+
+import UIKit
+
+class AuthViewController: TodoBaseController {
+   private(set) lazy var authModel = AuthModel()
+   private(set) lazy var authView: AuthView = AuthView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func setupLayout() {
+        self.view = authView
+        authView.mailTextField.delegate = self
+        authView.passwordTextField.delegate = self
+    }
+    
+    override func setupEvent() {
+        authView.loginButton.addTarget(self, action: #selector(loginEvent), for: .touchUpInside)
+    }
+    
+    
+    @objc func loginEvent(){
+        let mail = authView.mailTextField.text ?? ""
+        let password = authView.passwordTextField.text ?? ""
+        if (!mail.isEmpty && !password.isEmpty) {
+            authModel.login(mail: mail, password: password)
+        }
+    }
+}
+
+extension AuthViewController: UITextFieldDelegate {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           self.view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        return textField.resignFirstResponder()
+    }
+    
+}
---
+++ Todo/Todo/Controller/Base/TodoBaseController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by CHEN SINYU on 2020/08/16.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+
+import UIKit
+import Firebase
+class TodoBaseController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupLayout()
+        setupEvent()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    func setupLayout() {}
+    
+    func setupEvent() {}
+
+}
+
