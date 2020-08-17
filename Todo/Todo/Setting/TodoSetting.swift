932dbf029626ab1addda4016401a4f7cb582d85a
Ricky-yu <ts00484515@gmail.com>
2020-08-17 23:08:42 +0900

    add GobalSettingFile

---
+++ Todo/Todo/Setting/TodoSetting.swift
@@ -0,0 +1,28 @@
+//
+//  TodoSetting.swift
+//  Todo
+//
+//  Created by CHEN SINYU on 2020/08/17.
+//  Copyright © 2020 CHEN SINYU. All rights reserved.
+//
+import Foundation
+import UIKit
+import SnapKit
+
+let screenWidth = UIScreen.main.bounds.width
+let screenHeight = UIScreen.main.bounds.height
+
+extension UIColor {
+    class var background: UIColor {
+        return UIColor(red: 242/255, green: 242/255, blue: 242/255, alpha: 1.0)
+    }
+    
+    class var pink: UIColor {
+        return UIColor(red: 255/255, green: 128/255, blue: 168/255, alpha: 1.0)
+    }
+    
+    class var theme: UIColor {
+        return UIColor(red: 29/255, green: 221/255, blue: 43/255, alpha:1.0)
+    }
+}
+
